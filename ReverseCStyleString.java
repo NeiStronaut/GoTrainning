557b3e3d4689fc7bdaaf6502bdaf8a004b9cd1e8
NeiStronaut <37004652+NeiStronaut@users.noreply.github.com>
2018-03-06 11:31:36 -0600

    Add files via upload

---
+++ ReverseCStyleString.java
@@ -0,0 +1,32 @@
+package crackingcodinginterview;
+
+public class ReverseCStyleString {
+	
+	public static void reverse(String input) throws Exception 
+	{	
+		//reverse of null is null
+		if(input == null || input.isEmpty()) {
+			return;
+		}
+		char[] characters = input.toCharArray();
+
+		//Complexity O(n-1)
+		for(int i = characters.length-2; i >= 0; i--) {			
+			System.out.print(characters[i]);
+		}
+		System.out.print(characters[characters.length-1]);
+		System.out.println();
+	}
+	
+	
+	public static void main (String[] args) throws Exception 
+	{
+		String[] informalTestCases = {"a\0", "ab\0", "aba\0", "abcdefgh\0", "!.ñadf\0"};
+		
+		for(String tc : informalTestCases) {
+			System.out.println(tc);
+			reverse(tc);
+		}
+	}
+
+}
